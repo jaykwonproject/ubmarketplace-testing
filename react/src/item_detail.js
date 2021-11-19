f775baf7e780218cd00646a5a184f1a3a274f78c
ChangHoSung <changhos@buffalo.edu>
2021-11-19 04:41:48 -0500

    Update item_detail.js

--- react/src/item_detail.js
+++ react/src/item_detail.js
@@ -8,6 +8,7 @@ class Item_Detail extends Component{
     constructor(props) {
         super();
         this.state ={
+            isLoaded: false,
             item: null,
             relatedItems: null,
             id: null
@@ -64,7 +65,8 @@ class Item_Detail extends Component{
                         let otherItems = tmp2;
 
                         this.setState({
-                            relatedItems: otherItems
+                            relatedItems: otherItems,
+                            isLoaded: true
                         })
                     });
                 }
@@ -72,7 +74,7 @@ class Item_Detail extends Component{
     }
 
     render() {
-        let{item,relatedItems} = this.state;
+        let{isLoaded,item,relatedItems} = this.state;
 
         let editLink = `/item/edit/${this.state.id}`;
 
@@ -85,63 +87,122 @@ class Item_Detail extends Component{
                 <p>${item.price}</p>
             </div>
         )) : null;
-
-        return (
-            <div className="home">
-                <div className="header">
-                    <Link to="/">
-                        <p className="title">UB Marketplace</p>
-                    </Link>
-                    <div className="dropdown">
-                        <button className="dropbtn"></button>
-                        <div className="dropdown-content">
-                            <Link to="/login">Login/Register</Link>
-                            <Link to="/profile">View Profile</Link>
-                            <Link to="/">Logout</Link>
+        if(isLoaded == false){
+            return (
+                <div className="home">
+                    <div className="header">
+                        <Link to="/">
+                            <p className="title">UB Marketplace</p>
+                        </Link>
+                        <div className="dropdown">
+                            <button className="dropbtn"></button>
+                            <div className="dropdown-content">
+                                <Link to="/login">Login/Register</Link>
+                                <Link to="/profile">View Profile</Link>
+                                <Link to="/">Logout</Link>
+                            </div>
                         </div>
                     </div>
+                    {item ?
+                        <div className="itemInfo">
+                            <div className="itemLeft">
+                                <div className="itemThumbnail">
+                                    <img src={item.images[0]} alt={item.name} />
+                                </div>
+                            </div>
+                            <div className="itemRight">
+                                <div className="itemName">
+                                    <h2>{item.name} {item.owner.userId === localStorage.getItem("email") ? (<Link to={editLink}>Edit Item</Link>) : ''}</h2>
+                                </div>
+                                <div className="itemPrice">
+                                    <h3>Price: ${item.price}</h3>
+                                </div>
+                                <div className="itemLocation">
+                                    <h3>Available Meetup Location</h3>
+                                    <button>{item.meetingPlace}</button>
+                                </div>
+                                <h3 className="descriptionTitle">Item Description</h3>
+                                <div className="itemDescription">
+                                    {item.description}
+                                </div>
+                                <h3 className="descriptionTitle">Contact Info</h3>
+                                <div className="itemContact">
+                                    {item.contactPhoneNumber ? `${item.owner.userId} || ${item.contactPhoneNumber}` : item.owner.userId }
+                                </div>
+                            </div>
+                        </div>
+                        :
+                        <p>Loading...</p>
+                    }
+                    { related ?
+                        <div className="relatedList">
+                            <div className="relatedItems">
+                                <h3>Related Items</h3>
+                            </div>
+                            <p>Loading...</p>
+                        </div>
+                        : '' }
                 </div>
-                {item ?
-                    <div className="itemInfo">
-                        <div className="itemLeft">
-                            <div className="itemThumbnail">
-                                <img src={item.images[0]} alt={item.name} />
-                            </div>
-                        </div>
-                        <div className="itemRight">
-                            <div className="itemName">
-                                <h2>{item.name} {item.owner.userId === localStorage.getItem("email") ? (<Link to={editLink}>Edit Item</Link>) : ''}</h2>
-                            </div>
-                            <div className="itemPrice">
-                                <h3>Price: ${item.price}</h3>
-                            </div>
-                            <div className="itemLocation">
-                                <h3>Available Meetup Location</h3>
-                                <button>{item.meetingPlace}</button>
-                            </div>
-                            <h3 className="descriptionTitle">Item Description</h3>
-                            <div className="itemDescription">
-                                {item.description}
-                            </div>
-                            <h3 className="descriptionTitle">Contact Info</h3>
-                            <div className="itemContact">
-                                {item.contactPhoneNumber ? `${item.owner.userId} || ${item.contactPhoneNumber}` : item.owner.userId }
+            );
+        }
+        else{
+            return (
+                <div className="home">
+                    <div className="header">
+                        <Link to="/">
+                            <p className="title">UB Marketplace</p>
+                        </Link>
+                        <div className="dropdown">
+                            <button className="dropbtn"></button>
+                            <div className="dropdown-content">
+                                <Link to="/login">Login/Register</Link>
+                                <Link to="/profile">View Profile</Link>
+                                <Link to="/">Logout</Link>
                             </div>
                         </div>
                     </div>
-                : 
-                    <p>Loading...</p>
-                }
-                { related ? 
-                    <div className="relatedList">
-                        <div className="relatedItems">
-                            <h3>Related Items</h3>
+                    {item ?
+                        <div className="itemInfo">
+                            <div className="itemLeft">
+                                <div className="itemThumbnail">
+                                    <img src={item.images[0]} alt={item.name} />
+                                </div>
+                            </div>
+                            <div className="itemRight">
+                                <div className="itemName">
+                                    <h2>{item.name} {item.owner.userId === localStorage.getItem("email") ? (<Link to={editLink}>Edit Item</Link>) : ''}</h2>
+                                </div>
+                                <div className="itemPrice">
+                                    <h3>Price: ${item.price}</h3>
+                                </div>
+                                <div className="itemLocation">
+                                    <h3>Available Meetup Location</h3>
+                                    <button>{item.meetingPlace}</button>
+                                </div>
+                                <h3 className="descriptionTitle">Item Description</h3>
+                                <div className="itemDescription">
+                                    {item.description}
+                                </div>
+                                <h3 className="descriptionTitle">Contact Info</h3>
+                                <div className="itemContact">
+                                    {item.contactPhoneNumber ? `${item.owner.userId} || ${item.contactPhoneNumber}` : item.owner.userId }
+                                </div>
+                            </div>
                         </div>
-                        {related}
-                    </div>
-                : '' }
-            </div>
-        );
+                        :
+                        <p>Loading...</p>
+                    }
+                    { related ?
+                        <div className="relatedList">
+                            <div className="relatedItems">
+                                <h3>Related Items</h3>
+                            </div>
+                            {related}
+                        </div>
+                        : '' }
+                </div>
+            );
+        }
     }
 }
 
